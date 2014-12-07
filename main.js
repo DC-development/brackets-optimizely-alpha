a0a0b371df3b92d6b8e900b765d905e55aac718f
philipp.sengelmann <sengelmann.philipp@googlemail.com>
2014-12-07 04:05:55 +0100

    added css and js folders for optimizely-experiment-globals. Renamed some vars meaningful. Readded getProject step in loading, project folder structure is more complete now. Made styles be loaded and added Icon to Sidebar.

--- main.js
+++ main.js
@@ -5,28 +5,35 @@
 define(function (require, exports, module) {
     "use strict";
 
-
-
-    var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus          = brackets.getModule("command/Menus"),
-        ProjectManager = brackets.getModule("project/ProjectManager"),
+    var CommandManager  = brackets.getModule("command/CommandManager"),
+        Menus           = brackets.getModule("command/Menus"),
+        ProjectManager  = brackets.getModule("project/ProjectManager"),
         DocumentManager = brackets.getModule("document/DocumentManager"),
-        FileSystem = brackets.getModule("filesystem/FileSystem"),
-        FileUtils = brackets.getModule("file/FileUtils");
+        FileSystem      = brackets.getModule("filesystem/FileSystem"),
+        FileUtils       = brackets.getModule("file/FileUtils"),
+        ExtensionUtils  = brackets.getModule("utils/ExtensionUtils");
+ 
+    ExtensionUtils.loadStyleSheet(module, "style/styles.css");
 
-    // Function to run when the menu item is clicked
-    // 67f2538993cbc7a7a2c4b31e15a64ed0:8ec1c4e8
+    var $icon = $("<a id='optimizely-toolbar-icon' href='#'> </a>")
+        .attr("title", "Optimizely")
+        .appendTo($("#main-toolbar .buttons"));
+
+    $icon.on("click", handleHelloWorld);
+
     function handleHelloWorld() {
 
         var token = "";
         var projectName = "";
         var projectRoot = ProjectManager.getProjectRoot()._path;
-        var localPath = projectRoot+"optimizely.js";
-        var configFile = FileSystem.getFileForPath(localPath);
+        var configPath = projectRoot+"optimizely.js";
+        var configFile = FileSystem.getFileForPath(configPath);
         var projectId = "";
         var jsonObject;
         var readConfig;
-  
+        var experimentName;
+
+        
         // Read The Config File
         readConfig = FileUtils.readAsText(configFile);  // returns a promise
 
@@ -45,35 +52,32 @@ define(function (require, exports, module) {
             }
         });
 
-        /*/console.log(ProjectManager.addClassesProvider().fullPath());
+        //console.log(ProjectManager.addClassesProvider().fullPath());
         // Read Projects
-        $.ajax({
-            url: "https://www.optimizelyapis.com/experiment/v1/projects/",
-            type: "GET",
-            beforeSend: function(xhr){xhr.setRequestHeader('token', sec_token);},
-            success: function(data) { 
-                console.log("Project: "+ data[0]['project_name'] +" - "+ data[0]['id']);    
-                getProject(data[0]['id']);            
-                console.log("Project: "+ data[1]['project_name'] +" - "+ data[1]['id']);
-                getProject(data[1]['id']);
-            }
-        });
-        */
-        // Read A Projects Experiments
         function getProject(project_id){
+            $.ajax({
+                url: "https://www.optimizelyapis.com/experiment/v1/projects/"+project_id,
+                type: "GET",
+                beforeSend: function(xhr){xhr.setRequestHeader('token', token);},
+                success: function(data) {                  
+                    projectName = data.project_name;
+                    getExperiments(data.id);
+                }
+            });
+        }
+
+        // Read A Projects Experiments
+        function getExperiments(project_id){
             $.ajax({
                 url: "https://www.optimizelyapis.com/experiment/v1/projects/"+project_id+"/experiments/",
                 type: "GET",
                 beforeSend: function(xhr){xhr.setRequestHeader('token', token);},
-                success: function(mdata) { 
-                    console.log("Experiments: "+ mdata[0]['description']);
-                    console.log(mdata[0]);
-                    projectName =  mdata[0]['description'];
-                    getVariant(mdata[0].variation_ids[1]);
+                success: function(data) { 
+                    experimentName =  data[0]['description'];
+                    getVariant(data[0].variation_ids[1]);
                 }
             });
         }
-       
 
         // Read A  Experiments Variation
         function getVariant(variation_id){
@@ -82,12 +86,21 @@ define(function (require, exports, module) {
                 type: "GET",
                 beforeSend: function(xhr){xhr.setRequestHeader('token', token);},
                 success: function(data) { 
-                    console.log("Variation: "+ data.description);
-                    console.log(data.js_component); 
-                    FileSystem.getDirectoryForPath(projectRoot+projectName).create();
-                    // DocumentManager.getCurrentDocument().setText(data.js_component);
-                    console.log(projectRoot+projectName+"/"+data.description+".js");
-                    FileSystem.getFileForPath(projectRoot+projectName+"/"+data.description+".js").write(data.js_component);
+                    
+                    var variation = data.description;                    
+                    var projectDirectory =  FileSystem.getDirectoryForPath(projectRoot+projectName);
+                    var experimentDirectory =  FileSystem.getDirectoryForPath(projectRoot+projectName+"/"+experimentName);
+                    var experimentCssDirectory =  FileSystem.getDirectoryForPath(projectRoot+projectName+"/"+experimentName+"/css");
+                    var experimentJavaScriptDirectory =  FileSystem.getDirectoryForPath(projectRoot+projectName+"/"+experimentName+"/js");
+               
+                    var variantCode = FileSystem.getFileForPath(projectRoot+projectName+"/"+experimentName+"/"+variation+".js");
+                    projectDirectory.create();
+                    experimentDirectory.create();
+      
+                    experimentCssDirectory.create();
+                    experimentJavaScriptDirectory.create();
+                    
+                    variantCode.write(data.js_component);
                 }
             });
         }   
